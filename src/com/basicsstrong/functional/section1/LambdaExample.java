a8a0eea226725fab5820c7fa1c7db5f2fd53e54b
ediaz13 <diazemanuel27@gmail.com>
2023-04-12 01:52:34 -0300

    5. First Cute Lambda. Old Aproach II

--- src/com/basicsstrong/functional/section1/LambdaExample.java
+++ src/com/basicsstrong/functional/section1/LambdaExample.java
@@ -2,8 +2,12 @@ package com.basicsstrong.functional.section1;
 
 public class LambdaExample {
     public static void main(String[] args) {
-        MyRunnable runnable = new MyRunnable();
-        Thread thread = new Thread(runnable);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("New Thread!!");
+            }
+        });
         thread.start();
     }
 }
--- src/com/basicsstrong/functional/section1/MyRunnable.java
+++ src/com/basicsstrong/functional/section1/MyRunnable.java
@@ -1,8 +1,14 @@
 package com.basicsstrong.functional.section1;
-
+/*
 public class MyRunnable implements Runnable {
     @Override
     public void run() {
         System.out.println("Thread executed!!");
     }
 }
+ */
+
+
+
+
+
