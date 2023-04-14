8f99dca4e588e609ee180203fb4a66c40bf21adb
ediaz13 <diazemanuel27@gmail.com>
2023-04-14 11:53:11 -0300

    7. How Lambda Works Under the Hood

--- src/com/basicsstrong/functional/section1/LambdaUnderTheHood.java
+++ src/com/basicsstrong/functional/section1/LambdaUnderTheHood.java
@@ -2,12 +2,8 @@ package com.basicsstrong.functional.section1;
 
 public class LambdaUnderTheHood {
     public static void main(String[] args) {
-        MyFunInterface fun1 = new MyFunInterface() {
-            @Override
-            public void myMethod() {
-                System.out.println("Implementation 1");
-            }
-        };
+
+        MyFunInterface fun1 = () -> System.out.println("Implementation 1");
 
         MyFunInterface fun2 = new MyFunInterface() {
             @Override
@@ -15,5 +11,12 @@ public class LambdaUnderTheHood {
                 System.out.println("Implementation 2");
             }
         };
+
+
+
+        MyFunInterface fun = () -> System.out.println("Im Light Weight");
+
+
+
     }
 }
