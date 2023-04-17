34cbad1a75dd0b898e8b83dfdba06f5f7fca9d85
ediaz13 <diazemanuel27@gmail.com>
2023-04-17 20:52:29 -0300

    20. Consumer

---
+++ src/com/basicsstrong/functional/section5/ConsumerPractice.java
@@ -0,0 +1,22 @@
+package com.basicsstrong.functional.section5;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerPractice {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 2, 3, 45);
+
+        Consumer<Integer> consumer = e -> System.out.println(e);
+
+        consumer.accept(56);
+
+        printElements (list, consumer);
+    }
+
+    private static <T> void printElements(List<T> list, Consumer<T> consumer) {
+        for (T t: list ) {
+            consumer.accept(t);
+        }
+    }
+}
