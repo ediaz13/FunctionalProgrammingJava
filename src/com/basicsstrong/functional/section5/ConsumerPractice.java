793efda1d92452c9c9e07f08c484ff3740217342
ediaz13 <diazemanuel27@gmail.com>
2023-04-19 19:46:11 -0300

    20. Consumer

--- src/com/basicsstrong/functional/section5/ConsumerPractice.java
+++ src/com/basicsstrong/functional/section5/ConsumerPractice.java
@@ -14,7 +14,7 @@ public class ConsumerPractice {
         printElements (list, consumer);
     }
 
-    //Comentario guay.
+    //Comentario.
     private static <T> void printElements(List<T> list, Consumer<T> consumer) {
         for (T t: list ) {
             consumer.accept(t);
