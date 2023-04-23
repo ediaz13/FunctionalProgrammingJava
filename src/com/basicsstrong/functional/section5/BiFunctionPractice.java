f874e40a8ed68d8ab9c56e327d5fdf13700ce87b
ediaz13 <diazemanuel27@gmail.com>
2023-04-23 20:22:28 -0300

    24. BiFunction

---
+++ src/com/basicsstrong/functional/section5/BiFunctionPractice.java
@@ -0,0 +1,13 @@
+package com.basicsstrong.functional.section5;
+
+import java.util.function.BiFunction;
+
+public class BiFunctionPractice {
+    public static void main(String[] args) {
+        //BiFunction<String, String, String> biFunction = (a, b) -> a + b;
+        BiFunction<String, String, Integer> biFunction = (a, b) -> (a + b).length();
+
+        System.out.println(biFunction.apply("Basics","Bitches"));
+
+    }
+}
