ca56f784b60a970f8eb3eba6b3d3ddac7418c3cd
ediaz13 <diazemanuel27@gmail.com>
2023-08-11 12:14:42 -0300

    36. Operators Part - 2

--- src/com/basicsstrong/functional/section7/MoreOperations.java
+++ src/com/basicsstrong/functional/section7/MoreOperations.java
@@ -11,5 +11,23 @@ public class MoreOperations {
 
         //ifPresentOrElse
         Optional.empty().ifPresentOrElse(System.out:: println, () -> System.out.println("Value is Absent"));
+
+        //stream
+        optional.stream().forEach(System.out::println);
+        Optional.empty().stream().forEach(System.out::println);
+
+        //or
+        //optional.or(() -> Optional.of("New Value!")).ifPresent(System.out::println);
+        Optional.empty().or(() -> Optional.of("New Value!")).ifPresent(System.out::println);
+
+        //equals
+        //optional
+        //either both are empty
+        //or if the values in optional are equal to each other via equals method
+
+        System.out.println(optional.equals(Optional.of("Value")));
+
+        //hashcode
+        System.out.println(optional.hashCode());
     }
 }
