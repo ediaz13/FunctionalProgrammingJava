5b7af32f903d092d9fcabcd5f7aaac5aec474d68
ediaz13 <diazemanuel27@gmail.com>
2023-08-26 17:00:52 -0300

    60. Builder Design Pattern

---
+++ src/com/soma/functional/section9/BuilderDemo.java
@@ -0,0 +1,17 @@
+package com.soma.functional.section9;
+
+public class BuilderDemo {
+    public static void main(String[] args) {
+
+        MobileBuilder builder = new MobileBuilder();
+
+        Mobile myMobile = builder.with(myBuilder -> {
+            myBuilder.ram = 4;
+            myBuilder.battery = 4000;
+            myBuilder.processor = "A12 Bionic";
+        }).createMobile();
+
+        System.out.println(myMobile);
+
+    }
+}
