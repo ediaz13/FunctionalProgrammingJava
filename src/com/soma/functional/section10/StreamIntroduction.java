933d551d91c7e89c329448d8f03f40877e2f4e86
ediaz13 <diazemanuel27@gmail.com>
2023-08-28 19:41:05 -0300

    68- Filter Operations

--- src/com/soma/functional/section10/StreamIntroduction.java
+++ src/com/soma/functional/section10/StreamIntroduction.java
@@ -3,11 +3,19 @@ package com.soma.functional.section10;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamIntroduction {
     public static void main(String[] args) {
 
         List<Book> books = new ArrayList<>();
+
+        books.add(new Book("The Alchemist", "Paulo Coelho", "Adventure", 4.408789797));
+        books.add(new Book("The Notebook", "Nicholas Sparks", "Romance", 4.10));
+        books.add(new Book("Horror Cocktail", "Robert Bloch", "Horror", 2.67));
+        books.add(new Book("House of Leaves", "Mark Z. Danielewski", "Horror", 4.10908908));
+
+        Stream<Book> stream = books.stream();
 
         List<Book> popularHorrorBooks = new ArrayList<>();
 
@@ -25,8 +33,6 @@ public class StreamIntroduction {
                 .filter((book)-> book.getGenre().equalsIgnoreCase("Horror"))
                 .filter((book)-> book.getRating() > 3)
                 .collect(Collectors.toList());
-
-
 
 
     }
