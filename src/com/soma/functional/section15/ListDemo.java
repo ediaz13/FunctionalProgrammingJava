eba013bb765c603530dd067a70fb6fe4a4f6c739
ediaz13 <diazemanuel27@gmail.com>
2023-09-12 11:25:21 -0300

    106. List Remove

--- src/com/soma/functional/section15/ListDemo.java
+++ src/com/soma/functional/section15/ListDemo.java
@@ -4,5 +4,16 @@ public class ListDemo {
     public static void main(String[] args) {
         ListFun<Integer> list = ListFun.list(3, 6, 9);
         list.forEach(System.out::println);
+
+        System.out.println("xxx");
+
+        ListFun<Integer> integerListFun = list.addEle(56);
+        integerListFun.forEach(System.out::println);
+
+        System.out.println("xxx");
+
+        ListFun<Integer> removeEle = integerListFun.removeEle(9);
+        System.out.println();
+        removeEle.forEach(System.out::println);
     }
 }
